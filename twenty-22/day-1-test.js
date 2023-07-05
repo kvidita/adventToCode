f682ac00d8d6ef3fdecb9f60141f98657b1306d0
Vidita Kumari <kvidita@users.noreply.github.com>
2023-07-05 09:43:54 +0530

    tested toNumber

--- twenty-22/day-1-test.js
+++ twenty-22/day-1-test.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 const { describe, it } = require("node:test");
-const { splitByBlankLines, splitByLines } = require("./day-1");
+const { splitByBlankLines, splitByLines, toNumber } = require("./day-1");
 
 describe("splitByBlankLines", () => {
   it("should split text through blank lines", () => {
@@ -8,14 +8,14 @@ describe("splitByBlankLines", () => {
     const actual = ["line seperated by", "blank line"];
     const expected = splitByBlankLines(text);
     assert.deepStrictEqual(actual, expected);
-  })
+  });
 
   it("should split empty text through blank lines", () => {
     const text = "\n\n";
     const actual = ["", ""];
     const expected = splitByBlankLines(text);
     assert.deepStrictEqual(actual, expected);
-  })
+  });
 });
 
 describe("splitByLines", () => {
@@ -24,13 +24,28 @@ describe("splitByLines", () => {
     const actual = ["", ""];
     const expected = splitByLines(text);
     assert.deepStrictEqual(actual, expected);
-  })
+  });
 
   it("should split text by new line character", () => {
     const text = "text seperated by\nnew line character";
     const actual = ["text seperated by", "new line character"];
     const expected = splitByLines(text);
     assert.deepStrictEqual(actual, expected);
-  })
+  });
+});
 
-})
+describe("toNumber", () => {
+  it("should convert the stringified number into number", () => {
+    const stringifiedNum = "4";
+    const actual = 4;
+    const expected = toNumber(stringifiedNum);
+    assert.deepStrictEqual(actual, expected);
+  });
+
+  it("should not convert any string not containing number", () => {
+    const string = "j";
+    const actual = NaN;
+    const expected = toNumber(string);
+    assert.deepStrictEqual(actual, expected);
+  });
+});
