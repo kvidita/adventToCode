c2e5388c8679fde741491e30ce01b3bb642b52fe
Vidita Kumari <kvidita@users.noreply.github.com>
2023-07-06 18:59:30 +0530

    created functions: findSlackArea, findSurfaceArea and parseDimensions

--- twenty-15/day-2/src/gift-wrapper.js
+++ twenty-15/day-2/src/gift-wrapper.js
@@ -1,5 +1,27 @@
-const findWrapperArea = (boxDimensions) => {
-  return 2;
+const toNumber = (number) => +number;
+
+const parseDimensions = (rawDimension) => {
+  return rawDimension.split("x").map(toNumber);
 };
 
-exports.findWrapperArea = findWrapperArea;
+const findSurfaceArea = (dimensions) => {
+  const [l, b, h] = dimensions;
+  return 2 * (l * b + b * h + h * l);
+};
+
+const findSlackArea = (dimensions) => {
+  const [min1, min2] = dimensions.sort((a, b) => a - b);
+  return min1 * min2;
+};
+
+const findWrapperArea = (boxDimensions) => {
+  const dimensions = parseDimensions(boxDimensions);
+  const boxSurfaceArea = findSurfaceArea(dimensions);
+  const slack = findSlackArea(dimensions);
+
+  return boxSurfaceArea + slack;
+};
+
+findWrapperArea("1x1x1")
+
+module.exports = { findWrapperArea, parseDimensions, findSurfaceArea, findSlackArea };
--- twenty-15/day-2/test/gift-wrapper-test.js
+++ twenty-15/day-2/test/gift-wrapper-test.js
@@ -1,9 +1,33 @@
 const assert = require("assert");
 const { describe, it } = require("node:test");
-const { findWrapperArea } = require("../src/gift-wrapper.js");
+const { findWrapperArea, parseDimensions, findSurfaceArea, findSlackArea } = require("../src/gift-wrapper.js");
 
-describe("findWrapperArea", () => {
-  it("should give the surface area of the right rectangular prism of sides 1foot", () => {
-    assert.deepEqual(2, findWrapperArea("1x1x1"));
-  })
-})
+describe("wrapper", () => {
+  describe("findWrapperArea", () => {
+    it("should give the wrapper area of the right rectangular prism of sides 1foot", () => {
+      assert.deepEqual(findWrapperArea("1x1x1"), 7);
+    });
+  });
+
+  describe("parseDimensions", () => {
+    it("should parse the raw dimensions", () => {
+      assert.deepEqual(parseDimensions("1x1x1"), ["1", "1", "1"]);
+    });
+  });
+
+  describe("findSurfaceArea", () => {
+    it("should give the surface area of a right rectangular prism of side 1 foot each", () => {
+      assert.deepEqual(findSurfaceArea([1, 1, 1]), 6);
+    });
+  });
+
+  describe("findSlackArea", () => {
+    it("should give the slack area required", () => {
+      assert.deepEqual(findSlackArea([1, 1, 1]), 1);
+    });
+
+    it("should give the slack area for a cuboid of different dimensions", () => {
+      assert.deepEqual(findSlackArea([1, 2, 3]), 2);
+    });
+  });
+});
